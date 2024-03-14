54bdc31c0cec27eceffe5219e6a039838147ea8f
Alex Yashin <alex-yashin@ya.ru>
2024-03-14 09:31:50 +0400

    fix

--- src/Controls/MediaView.php
+++ src/Controls/MediaView.php
@@ -10,10 +10,16 @@ use Pina\Html;
 class MediaView extends Control
 {
     protected $url = '';
+    protected $type = '';
 
     public function setUrl($url)
     {
         $this->url = $url;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
     }
 
     protected function draw()
