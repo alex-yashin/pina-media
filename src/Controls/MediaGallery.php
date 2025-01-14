5a7ad43f28e79437ce7dbcc7c4182be5409f9811
Alex Yashin <alex-yashin@ya.ru>
2025-01-14 19:13:28 +0400

    переношу скрипт сортировки локально

--- src/Controls/MediaGallery.php
+++ src/Controls/MediaGallery.php
@@ -11,7 +11,7 @@ class MediaGallery extends MediaControl
 
     protected function makeScriptContent()
     {
-        App::assets()->addScript('https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js');
+        App::assets()->addScript('vendor/sortable.min.js');//https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js
         $encoded = json_encode($this->value, JSON_UNESCAPED_UNICODE);
         $sortable = "new Sortable($this->tagId.querySelector('.thumbnails'), {animation: 150,ghostClass: 'blue-background-class'})";
         return "MultipleMediaControl('#" . $this->tagId . "', '" . $this->name . "[]', " . $encoded . ");" . $sortable;
